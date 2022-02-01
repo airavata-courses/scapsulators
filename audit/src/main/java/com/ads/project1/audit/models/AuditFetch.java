38c5092ebb22d328a34ce0b40573cd8eac8628e1
Rutuja Jadhav <rsjadhav@iu.edu>
2022-02-01 15:36:07 -0500

    Audit micriservice code : Issue 15(https://github.com/airavata-courses/scapsulators/issues/15)

---
+++ audit/src/main/java/com/ads/project1/audit/models/AuditFetch.java
@@ -0,0 +1,55 @@
+package com.ads.project1.audit.models;
+
+public class AuditFetch {
+	private String date;
+	
+	private String time;
+	
+	private String nexradStation;
+	
+	private String currentDate;
+
+	public String getCurrentDate() {
+		return currentDate;
+	}
+
+	public void setCurrentDate(String currentDate) {
+		this.currentDate = currentDate;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	
+
+	public AuditFetch(String date, String time, String nexradStation, String currentDate) {
+		super();
+		this.date = date;
+		this.time = time;
+		this.nexradStation = nexradStation;
+		this.currentDate = currentDate;
+	}
+	
+	public AuditFetch() {}
+
+	public String getNexradStation() {
+		return nexradStation;
+	}
+
+	public void setNexradStation(String nexradStation) {
+		this.nexradStation = nexradStation;
+	}
+}
