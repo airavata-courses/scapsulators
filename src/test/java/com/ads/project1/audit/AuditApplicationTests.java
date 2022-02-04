0c8be7ccea2646a547b41dc139edc5ac6ec27fe5
Rutuja Jadhav <rsjadhav@iu.edu>
2022-02-04 15:33:35 -0500

    Adding all the files outside so that Circle CI can build the application - Issue 33(https://github.com/airavata-courses/scapsulators/issues/33)

---
+++ src/test/java/com/ads/project1/audit/AuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.project1.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
