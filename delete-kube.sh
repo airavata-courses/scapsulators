58cef4f4aa3dbe937f1d99805eb5528622abf7ae
Shubham Mohapatra <shubham.mohapatra1996@gmail.com>
2022-02-26 20:28:53 -0500

    adding kubernetes orchestration #71

---
+++ delete-kube.sh
@@ -0,0 +1,2 @@
+cd kubernetes
+kubectl delete -f audit.yaml,authenticate.yaml,database-connect.yaml,gateway.yaml,mongodb.yaml,react.yaml,weather.yaml
---
+++ start-kube.sh
@@ -0,0 +1,2 @@
+cd kubernetes
+kubectl apply -f audit.yaml,authenticate.yaml,database-connect.yaml,gateway.yaml,mongodb.yaml,react.yaml,weather.yaml
