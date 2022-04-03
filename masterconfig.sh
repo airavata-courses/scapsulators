0248f80359227ac9e4a7ec7156dea593e182a85a
Shubham Mohapatra <shubham.mohapatra1996@gmail.com>
2022-04-03 19:17:03 -0400

    adding config files for CD #83

--- masterconfig.sh
+++ masterconfig.sh
@@ -2,7 +2,7 @@
 sudo su -
 privateIp=$(hostname -I | awk '{print $1}')
 
-kubeadm init --apiserver-advertise-address=$privateIp --pod-network-cidr=192.168.0.0/16  --ignore-preflight-errors=all
+kubeadm init --apiserver-advertise-address=$privateIp --pod-network-cidr=10.0.0.0/16  --ignore-preflight-errors=all
 
 kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
 
