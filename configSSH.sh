a8dc74eb53cc22c16d0d69979420eccbb0690b2d
Shubham Mohapatra <shubham.mohapatra1996@gmail.com>
2022-04-03 19:03:27 -0400

    adding config files for CD #83

--- configSSH.sh
+++ configSSH.sh
@@ -4,10 +4,10 @@ workerip=$(openstack server list | grep scapsulators-worker | awk '{print $9}')
 
 echo $masterip $workerip
 
-ssh -i ~/.ssh/scapsulators ubuntu@$masterip -q 'bash -s' < serverconfig.sh
-ssh -i ~/.ssh/scapsulators ubuntu@$workerip -q 'bash -s' < serverconfig.sh
+ssh -i ~/.ssh/scapsulators -oStrictHostKeyChecking=no ubuntu@$masterip -q 'bash -s' < serverconfig.sh
+ssh -i ~/.ssh/scapsulators -oStrictHostKeyChecking=no ubuntu@$workerip -q 'bash -s' < serverconfig.sh
 
-ssh -i ~/.ssh/scapsulators ubuntu@$masterip -q 'bash -s' < masterconfig.sh
+ssh -i ~/.ssh/scapsulators -oStrictHostKeyChecking=no ubuntu@$masterip -q 'bash -s' < masterconfig.sh
 
-token=$(ssh -i ~/.ssh/scapsulators ubuntu@$masterip -q "sudo kubeadm token create --print-join-command 2>/dev/null" )
-ssh -i ~/.ssh/scapsulators ubuntu@$workerip sudo $token
+token=$(ssh -i ~/.ssh/scapsulators -oStrictHostKeyChecking=no ubuntu@$masterip -q "sudo kubeadm token create --print-join-command 2>/dev/null" )
+ssh -i ~/.ssh/scapsulators -oStrictHostKeyChecking=no ubuntu@$workerip sudo $token
