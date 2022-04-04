3f366fabd34a33642b6cef22145fd2a554e549bf
Shubham Mohapatra <shubham.mohapatra1996@gmail.com>
2022-04-04 17:24:18 -0400

    changes in file #81

---
+++ app-cred-shubham-openstack-openrc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export OS_AUTH_TYPE=v3applicationcredential
+export OS_AUTH_URL=https://js2.jetstream-cloud.org:5000/v3/
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME="IU"
+export OS_INTERFACE=public
+export OS_APPLICATION_CREDENTIAL_ID=4f6c452bc2b9404798e9b1dda4e0ad96
+export OS_APPLICATION_CREDENTIAL_SECRET=shubham123
