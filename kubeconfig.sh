1170de0917db13db11d6132a60c0f9848a03781c
Shubham Mohapatra <shubham.mohapatra1996@gmail.com>
2022-04-04 19:56:37 -0400

    adding kubespray config #81

--- kubeconfig.sh
+++ kubeconfig.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+masterip=$(openstack server list | grep scapsulators-master | awk '{print $9}')
 master_private_ip=$(openstack server list | grep scapsulators-master | awk '{print $8}' | cut -b 22- | rev | cut -b 2- | rev)
 worker_private_ip=$(openstack server list | grep scapsulators-worker | awk '{print $8}' | cut -b 22- | rev | cut -b 2- | rev)
 
