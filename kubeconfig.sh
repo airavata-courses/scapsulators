638cb352fa45198a877a2ba63c4fb2e339a1a891
Shubham Mohapatra <shubham.mohapatra1996@gmail.com>
2022-04-04 20:00:50 -0400

    adding kubespray config #81

--- kubeconfig.sh
+++ kubeconfig.sh
@@ -5,6 +5,6 @@ master_private_ip=$(openstack server list | grep scapsulators-master | awk '{pri
 worker_private_ip=$(openstack server list | grep scapsulators-worker | awk '{print $8}' | cut -b 22- | rev | cut -b 2- | rev)
 
 
-scp -i ~/.ssh/scapsulators scapsulators ubuntu@$masterip:~/id_rsa
+scp -i ~/.ssh/scapsulators ~/.ssh/scapsulators -oStrictHostKeyChecking=no ubuntu@$masterip:~/id_rsa
 ssh -i ~/.ssh/scapsulators -oStrictHostKeyChecking=no ubuntu@$masterip -q 'bash -s' < masterconfig.sh $master_private_ip $worker_private_ip
 
