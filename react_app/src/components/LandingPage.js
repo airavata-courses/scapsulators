417a53ac7d4391bfd83fc69c9289855141760627
Shubham Mohapatra <84135718+shubhpatr@users.noreply.github.com>
2022-04-05 15:17:33 -0400

    Update LandingPage.js

--- react_app/src/components/LandingPage.js
+++ react_app/src/components/LandingPage.js
@@ -30,7 +30,7 @@ export default function LandingPage() {
         <source src={BackgroundVideo} type="video/mp4" />
         </video>
             
-            <h1 className="main-title text-center"><span className="main-titlespan">Weatherpedia</span></h1>
+            <h1 className="main-title text-center"><span className="main-titlespan">Weatherpedia 2.0</span></h1>
             <p className="main-para text-center"><span className="main-titlespan">An online weather investigation tool</span></p>
             <div className='container-home'>
             <LoginPage />
